4c27185c286f4cf8ce2529278e24d4db2b99a4e6
Tom McGrath <thomas.m.mcgrath@gmail.com>
2014-01-22 22:16:12 +0000

    added cell creation code

--- gillespieHeteroplasmy.py
+++ gillespieHeteroplasmy.py
@@ -17,7 +17,9 @@ cellCycleTime = 3000
 population = {}
 
 # setup initial population
-population[(1,1)] = (1, coll.deque([0]))
+population[(1,1)] = [1, coll.deque([0])]
+population[(2,1)] = [1, coll.deque([1])]
+population[(1,2)] = [1, coll.deque([2])]
 
 # return shortest time to deterministic division event
 def getShortestClock(population):
@@ -25,9 +27,10 @@ def getShortestClock(population):
     for x in population.keys():
         if population[x][1][0] < shortestClock:
             shortestClock = population[x][1][0]
+            populationMember = x
         else:
             continue
-    return shortestClock
+    return (shortestClock, populationMember)
 
 # transition functions
 def probAPlusOne((numA, numB)):
@@ -42,20 +45,48 @@ def probBPlusOne((numA, numB)):
     
 def chooseDivisiom((numA, numB)):
     numALost = np.random.binomial(numA, 0.5)
-    numBLost = np.random.binomial(numB, 0.5)
-    
+    numBLost = np.random.binomial(numB, 0.5)    
     return ((numA-numALost, numB-numBLost),(numALost, numBLost))
+    
+def createCell((numA, numB)):
+    try:    
+        population[(numA, numB)][0] += 1
+        population[(numA, numB)][1].append(cellCycleTime + np.random.uniform()) # init with some random delta to prevent collisions
+    except KeyError:
+        population[(numA, numB)] = [1]
+        population[(numA, numB)].append(coll.deque([cellCycleTime + np.random.uniform()]))
+    
+# the Gillespie loop:
+def run(runTime):
+    t = 0
+    while t < runTime:
+        a0 = calcA0(population)
+        tau = np.random.exponential(float(1/a0))
+        
+        if tau < getShortestClock(population)[0]:
+            # partition cell from shortest clock cell group
+            # create new cells with appropriate mito numbers (initialise cell clocks w/some randomness to prevent collision)
+            # delete old cell
+            # advance time by shortest clock time
+            print 'not here yet!'
+            
+        elif tau >= getShortestClock(population)[0]:
+            # partition [0, 1) appropriately by a_i
+            # draw from [0, 1)
+            # do the appropriate action
+            print 'not here yet either!'
+        
     
 # calculate a0
 def calcA0(population):
     # loop over each element of the population dictionary and sum the total probabilities
-    a0 = 0.0    
+    returnVar = 0.0    
     for x in population.keys():
         #print x #useful to see the workings here
-        a0 += probAPlusOne(x)
+        returnVar += probAPlusOne(x)
         #print a0
-        a0 += probBPlusOne(x)
+        returnVar += probBPlusOne(x)
         #print a0
-    return a0
+    return returnvar
         
             
